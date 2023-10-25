3be167792c2731b57e8c6dd02f844a93de85e2d5
Mohamed Abubaker <67129352+mohamedlecter@users.noreply.github.com>
2023-10-25 17:40:02 +0800

    EfficientNetB0 model

--- server/server.py
+++ server/server.py
@@ -1,37 +1,18 @@
 import os
-import numpy as np
-from flask import Flask, request, render_template, jsonify
-from tensorflow import keras
+from flask import Flask, request, render_template
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from werkzeug.utils import secure_filename
-from PIL import Image
+from tensorflow.keras.applications.efficientnet import preprocess_input
 import json
 
-
-app = Flask(__name__, template_folder="templates", static_url_path='/static') 
+app = Flask(__name__)
 
 # Load the trained model
-model = keras.models.load_model('model/models/plant_disease_detector')
+model = tf.keras.models.load_model('model 2/models/plant_disease_detector')  # Replace 'model_path' with the actual path to your saved model
 
-# Load class_name_lookup from the JSON file
-with open('model/class_name_lookup.json', 'r') as f:
-    class_name_lookup = json.load(f)
-
-# Define the allowed file extensions
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# Define a function to preprocess the image
-
-def preprocess_image(image):
-    # Resize the image to the input size expected by the model (256x256)
-    img = image.resize((256, 256))
-    img = tf.keras.applications.mobilenet_v2.preprocess_input(np.array(img))
-    img = np.expand_dims(img, axis=0)  # Add a batch dimension
-    return img
+with open('model 2/class_names.json', 'r') as f:
+    class_names = json.load(f)
 
 @app.route('/')
 def index():
@@ -39,31 +20,36 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Get the uploaded image from the request
-        image_file = request.files['image']
-        if image_file:
-            image = Image.open(image_file)
+    if 'image' not in request.files:
+        return "No image file provided."
 
-            # Preprocess the image
-            preprocessed_image = preprocess_image(image)
+    image = request.files['image']
 
-            # Make predictions using your plant disease detection model
-            predictions = model.predict(preprocessed_image)
+    if image.filename == '':
+        return "No selected file."
 
-            # Process the predictions as needed
-            class_index = np.argmax(predictions)
-            class_name = class_name_lookup[str(class_index)]
-            class_name = class_name.replace('_', ' ')
+    if image:
+        # Save the uploaded image to a temporary file
+        image_path = 'temp.jpg'
+        image.save(image_path)
 
-            return f"Predicted class: {class_name}"
-        else:
-            return "No image file found in the request", 400
-    except Exception as e:
-        return str(e), 500
+        # Load and preprocess the input image
+        input_image = load_img(image_path, target_size=(224, 224))
+        input_image = img_to_array(input_image)
+        input_image = preprocess_input(input_image)
+        input_image = np.expand_dims(input_image, axis=0)  # Add a batch dimension
 
+        # Make predictions
+        predictions = model.predict(input_image)
 
+        # Get the predicted class
+        predicted_class_index = np.argmax(predictions)
+        predicted_class = class_names[predicted_class_index]
+
+        # Clean up temporary file
+        os.remove(image_path)
+
+        return f'Predicted class: {predicted_class}'
 
 if __name__ == '__main__':
-    os.makedirs('uploads', exist_ok=True)
     app.run(debug=True)
